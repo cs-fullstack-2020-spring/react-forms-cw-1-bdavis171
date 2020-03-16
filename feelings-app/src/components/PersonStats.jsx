dd7d0546af4286310351edd5bf3c878659fdd34e
autumn-ragland <raglanda998@gmail.com>
2020-03-16 16:43:59 -0500

    graded

--- feelings-app/src/components/PersonStats.jsx
+++ feelings-app/src/components/PersonStats.jsx
@@ -1,20 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 
 class PersonStats extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            name : '',
-            age : '',
-            feeling : ''
+            name: '',
+            age: '',
+            feeling: ''
         }
     }
     //record name entry
     nameEntry = (event) => {
         this.setState(
             {
-                name : event.target.value
+                name: event.target.value
             }
         )
     }
@@ -23,7 +23,7 @@ class PersonStats extends Component {
     ageEntry = (event) => {
         this.setState(
             {
-                age : event.target.value
+                age: event.target.value
             }
         )
     }
@@ -32,50 +32,52 @@ class PersonStats extends Component {
     feelingEntry = (event) => {
         this.setState(
             {
-                feeling : event.target.value
+                feeling: event.target.value
             }
         )
     }
-//function for submit button
-  submitEntry = (event) => {
+    //function for submit button
+    submitEntry = (event) => {
         event.preventDefault();
-          //reference elements needed
+        //reference elements needed
         const log = document.getElementById("entrylog");
 
         //create template literal for entry log
         const entry = `Hello ${this.state.name}. Your age is ${this.state.age} and you're feeling ${this.state.feeling}.`
 
         //display entry in the DOM
-       return log.innerText = entry;
+        return log.innerText = entry;
     }
-    
+
 
     render() {
-        
-        return(
+
+        return (
             <div>
                 <form action="">
-                <fieldset>
-                    <legend>Your Stats</legend>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" id="name" onChange={this.nameEntry} value={this.state.name}/>
-                    <br/>
-                    <label htmlFor="age">Age:</label>
-                    <input type="text" id="age" onChange={this.ageEntry} value={this.state.age}/>
-                    <br/>
-                    <label htmlFor="feeling">Feeling:</label>
-                    <input type="text" id='feeling' onChange={this.feelingEntry} value={this.state.feeling}/>
-                    <br/>
-                    <button onClick={this.submitEntry}>Submit</button>
-                </fieldset>
+                    <fieldset>
+                        <legend>Your Stats</legend>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name="name" id="name" onChange={this.nameEntry} value={this.state.name} />
+                        <br />
+                        {/* would have made sense for this to be a number input */}
+                        <label htmlFor="age">Age:</label>
+                        <input type="text" id="age" onChange={this.ageEntry} value={this.state.age} />
+                        <br />
+                        {/* would have made sense for this to be a text area input */}
+                        <label htmlFor="feeling">Feeling:</label>
+                        <input type="text" id='feeling' onChange={this.feelingEntry} value={this.state.feeling} />
+                        <br />
+                        <button onClick={this.submitEntry}>Submit</button>
+                    </fieldset>
                 </form>
                 <div id="entrylog">
-                  
-                   </div>
-                   
+
+                </div>
+
             </div>
         )
-        
+
     }
 
 }
